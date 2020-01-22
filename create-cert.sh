6cb72ea1d0399b168dfa7d1e6ab36a6422b226c4
alvarepc <55838813+alvarepc@users.noreply.github.com>
2020-01-22 20:06:54 +0800

    Add files via upload

---
+++ create-cert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Generating a private key..."
+openssl genrsa -out ssl.key 2048
+echo 
+
+echo "Generating a CSR..."
+openssl req -new -key ssl.key -out ssl.csr -subj "/CN=my-apache.openshift.lab.example.com"
+echo
+
+echo "Generating a certificate..."
+openssl x509 -req -days 999 -in ssl.csr -signkey ssl.key -out ssl.crt
+echo
+echo "DONE."
+echo
